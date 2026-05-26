568bc25d38cdf93ad1b00b9da058fffdebe2f449
Akash <akashpillai40@gmail.com>
2026-05-26 16:55:34 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "../styles/App.css";
 
 function App() {
   return (
-    <div id="root">
     <p>I am learning React. My life is getting better.</p>
-    </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+export default App;
